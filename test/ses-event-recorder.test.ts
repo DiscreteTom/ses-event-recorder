aab4bd4aee7f8eb18b0a6847a5881163410d4486
DiscreteTom <discrete_tom@outlook.com>
2022-04-01 16:26:25 +0800

    cdk init

---
+++ test/ses-event-recorder.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SesEventRecorder from '../lib/ses-event-recorder-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ses-event-recorder-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SesEventRecorder.SesEventRecorderStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
